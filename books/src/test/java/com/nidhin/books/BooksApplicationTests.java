a39059849518bc786ebc1558aed8706350f4e36f
nidhinalunal <nidhinalungal@gmail.com>
2021-10-05 16:51:49 +0530

    added dockerfiles

--- books/src/test/java/com/nidhin/books/BooksApplicationTests.java
+++ books/src/test/java/com/nidhin/books/BooksApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BooksApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
--- library/src/test/java/com/nidhin/library/LibraryApplicationTests.java
+++ library/src/test/java/com/nidhin/library/LibraryApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LibraryApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
